70ca1034b6f4076e0dacc6afc83e420c08d4625d
fritzladwig <fritz.ladwig@yahoo.de>
2025-04-25 15:13:19 +0200

    Line endings messed with key reading.

--- authorization/src/main/java/edu/tum/ase/authorization/utils/JwkUtils.java
+++ authorization/src/main/java/edu/tum/ase/authorization/utils/JwkUtils.java
@@ -1,5 +1,6 @@
 package edu.tum.ase.authorization.utils;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
@@ -7,28 +8,35 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 public class JwkUtils {
 
     public static RSAPrivateKey readPrivateKey(String filePath) throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get(filePath));
-        String privateKeyPEM = new String(keyBytes)
+        String pemContent = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+
+        String base64Content = pemContent
                 .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replaceAll("\\n", "")
-                .replace("-----END PRIVATE KEY-----", "");
-        byte[] decoded = java.util.Base64.getDecoder().decode(privateKeyPEM);
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getMimeDecoder().decode(base64Content);
+
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return (RSAPrivateKey) kf.generatePrivate(spec);
     }
 
     public static RSAPublicKey readPublicKey(String filePath) throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get(filePath));
-        String publicKeyPEM = new String(keyBytes)
+        String pemContent = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
+
+        String base64Content = pemContent
                 .replace("-----BEGIN PUBLIC KEY-----", "")
-                .replaceAll("\\n", "")
-                .replace("-----END PUBLIC KEY-----", "");
-        byte[] decoded = java.util.Base64.getDecoder().decode(publicKeyPEM);
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getMimeDecoder().decode(base64Content);
+
         X509EncodedKeySpec spec = new X509EncodedKeySpec(decoded);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return (RSAPublicKey) kf.generatePublic(spec);
